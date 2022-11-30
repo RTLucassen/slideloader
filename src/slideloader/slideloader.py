b3deca77b3fd6d4c84889f65d6c2ec68d2e0fc4f
Ruben <54849762+RTLucassen@users.noreply.github.com>
2022-11-30 09:11:48 +0100

    Minor changes to copyright notice

--- src/slideloader/slideloader.py
+++ src/slideloader/slideloader.py
@@ -1,4 +1,4 @@
-#    Copyright 2022 Ruben Lucassen 
+#    Copyright 2022 Ruben T Lucassen, UMC Utrecht, The Netherlands 
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
